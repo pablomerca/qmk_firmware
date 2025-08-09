ff686659d0b880fcd197fc231b7da9688df018cc
Pablo Mercado <pablomercado0123@gmail.com>
2025-08-09 18:56:04 -0300

    adds config.h to pablo keymaps

---
+++ keyboards/handwired/dactyl_minidox/keymaps/pablo/config.h
@@ -0,0 +1,10 @@
+#pragma once
+
+// Force this half to be master
+// #define MASTER_LEFT
+// #define MASTER_RIGHT
+#define EE_HANDS  // for EEPROM-configurable side
+
+// Optional: enable/disable features
+// #undef RGB_MATRIX_ENABLE
+// #undef RGBLIGHT_ENABLE
